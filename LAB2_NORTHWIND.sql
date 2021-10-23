24895a93f1ab97805ecddef7a759c4fa6217176a
Marcel Spryszyński <81514773+MSpryszynski@users.noreply.github.com>
2021-10-23 22:01:05 +0200

    Update LAB2_NORTHWIND.sql

--- LAB2_NORTHWIND.sql
+++ LAB2_NORTHWIND.sql
@@ -1,35 +1,34 @@
-SELECT Phone + ', ' + ISNull(Fax, '') as phonefax FROM Suppliers
--- Zastosowania agregatów
+-- Zastosowania agregatÃ³w
 
--- 1. Podaj liczbê produktów o cenach mniejszych ni¿ 10$ lub wiêkszych ni¿ 20$
+-- 1. Podaj liczbÄ™ produktÃ³w o cenach mniejszych niÅ¼ 10$ lub wiÄ™kszych niÅ¼ 20$
 SELECT COUNT(*) FROM Products WHERE UnitPrice NOT BETWEEN 10 AND 20
 
--- 2. Podaj maksymaln¹ cenê produktu dla produktów o cenach poni¿ej 20$
+-- 2. Podaj maksymalnÄ… cenÄ™ produktu dla produktÃ³w o cenach poniÅ¼ej 20$
 SELECT Max(UnitPrice) FROM Products WHERE UnitPrice < 20 
 
--- 3. Podaj maksymaln¹, minimaln¹ i œredni¹ cenê produktu dla produktów sprzedawanych w butelkach (‘bottle’)
+-- 3. Podaj maksymalnÄ…, minimalnÄ… i Å›redniÄ… cenÄ™ produktu dla produktÃ³w sprzedawanych w butelkach (â€˜bottleâ€™)
 SELECT MAX(UnitPrice) as Maxprice, MIN(UnitPrice) as Minprice, AVG(UnitPrice) as avgprice 
 FROM Products WHERE QuantityPerUnit LIKE '%bottle%'
 
--- 4. Wypisz informacjê o wszystkich produktach o cenie powy¿ej œredniej
+-- 4. Wypisz informacjÄ™ o wszystkich produktach o cenie powyÅ¼ej Å›redniej
 SELECT * FROM Products WHERE UnitPrice > (SELECT AVG(UnitPrice) FROM Products)
 
--- 5. Podaj wartoœæ zamówienia o numerze 10250
+-- 5. Podaj wartoÅ›Ä‡ zamÃ³wienia o numerze 10250
 SELECT * FROM [Order Details] WHERE OrderID = 10250
 
 --Zastosowania GROUP BY
 
--- 1. Podaj maksymaln¹ cenê zamawianego produktu dla
--- ka¿dego zamówienia. Posortuj zamówienia wg maksymalnej ceny produktu
+-- 1. Podaj maksymalnÄ… cenÄ™ zamawianego produktu dla
+-- kaÅ¼dego zamÃ³wienia. Posortuj zamÃ³wienia wg maksymalnej ceny produktu
 SELECT OrderID, MAX(UnitPrice) as Maxprice FROM [Order Details] GROUP BY OrderID  ORDER BY Maxprice DESC
 
--- 2. Podaj maksymaln¹ i minimaln¹ cenê zamawianego produktu dla ka¿dego zamówienia
+-- 2. Podaj maksymalnÄ… i minimalnÄ… cenÄ™ zamawianego produktu dla kaÅ¼dego zamÃ³wienia
 SELECT OrderID, MAX(UnitPrice) as MAxprice, MIN(UnitPrice) as Minprice
 FROM [Order Details] GROUP BY OrderID
 
--- 3. Podaj liczbê zamówieñ dostarczanych przez poszczególnych spedytorów
+-- 3. Podaj liczbÄ™ zamÃ³wieÅ„ dostarczanych przez poszczegÃ³lnych spedytorÃ³w
 SELECT COUNT(*) as Orders, ShipVia FROM Orders GROUP BY ShipVia
 
--- 4. Który ze spedytorów by³ najaktywniejszy w 1997 roku?
+-- 4. KtÃ³ry ze spedytorÃ³w byÅ‚ najaktywniejszy w 1997 roku?
 SELECT TOP 1 ShipVia, COUNT(*) as Orders1997 FROM Orders WHERE YEAR(ShippedDate) = 1997
 GROUP BY ShipVia ORDER BY Orders1997 DESC
